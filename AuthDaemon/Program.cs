62429bc0ee7afe9065e1f0982d5699043dc159e4
Maxim <freepvps@gmail.com>
2016-03-10 05:18:16 +0300

    Update Program.cs

--- AuthDaemon/Program.cs
+++ AuthDaemon/Program.cs
@@ -3,6 +3,7 @@
 // Github: https://github.com/FreePVPs/AuthDaemon/tree/master/AuthDaemon
 // ======================================================================
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
